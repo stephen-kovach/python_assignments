8f3f19db438098c0ebf41f5f49313e15442b551a
stephen-kovach <smkovach5458@gmail.com>
2022-05-15 22:14:51 -0500

    demo 020

--- 18_counter/server.py
+++ 18_counter/server.py
@@ -25,42 +25,25 @@ def index():
 
 
     if "count" in session:
-        session["count"] +=1  # this will increment by 1
+        session["count"] +=1 # this will increment by 1
     else:
         session["count"] = 1
 
 # TOGGLE
     # return render_template("index.html", count = session["count"]) 
     # is ABOVE code hooked up to line 16, 18-a_counter index.html?
-    return render_template("z02_index.html", count = session["count"]) 
+    return render_template("z04_index.html", count = session["count"]) 
     # is ABOVE code hooked up to line 16, 18-counter z02_index.html?
 
 # BELOW KWV 22:40  are users designed to post catch data?.  NCV Methods with an 's'
-@app.route('/users', methods=["post"]) 
-            # ====>WHY does it say "Method not found" in browser
-            # ====> Me 6:00pm is it because it's what prints in terminal?
-def process_user():
-    print("Cato the Elder")
-    print(request.form)   #prints immutableMultiDict
-    print(request.form["username"])
-    # print(request.form["email"])  #---this doesn't work.  why not?
-    print("Yo!  This is the processing route")
-    print("Cato the Younger")
-    # print(f"{username} was here! 1")  --> this breaks the code
-    username = request.form["username"] # not in terminal.  what is this doing?
-    # email = request.form["email"] # Nope 12:27 So does this add [term] to the [term --terminal?] 
-    print("Don't promise twice what you can do at once.")
-    print(f"{username} was here! 2")
-    # print(f"I swear {email} is somewhere around here...")
+# @app.route('/users', methods=["post"]) 
+#             # ====>WHY does it say "Method not found" in browser
+#             # ====> Me 6:00pm is it because it's what prints in terminal?
 
-    # KWV 24:45 never render on a post route --on LP?  Where?
-
-    # 72 6:00 Added BELOW,  External Processing: I want to change the label  -what does that mean?
-    session["user"] = request.form["username"]
-    # What does above code do?
-    
-    # 72 8:42 new edit for 5ish below
-    return redirect("/show") #==> to line 71 server.py?
+# def process_user():
+   
+#     # 72 8:42 new edit for 5ish below
+#     return redirect("/show") #==> to line 71 server.py?
     
 
 # /////////////////////////////////////////////////
@@ -68,15 +51,15 @@ def process_user():
 # /////////////////////////////////////////////////
 
 # 72 9:15 added below -Don't forget, delibrately making errors
-@app.route("/show")
-def show_user_info():
-    print("?+?+?+?+?+?+?+?+?+?+?+?+?+?+?+?+?+?+?+?" + session["user"]) #10:00 forgot to put user in ""
-    # return render_template("show.html") #commented out at 72 15:17
-# 72 11:50 Session is empty by default.  12:15  Session is a big empty box,  it only stores something if we tell it to
-# 72 15:48 substitued 2 above for 1 below:
-    return render_template("show.html", user_name = session["user"]) 
-    # where is ABOVE user_name hooked up?
-    # Me 8:49pm is it lines 21 & 36, 18-b_counter show.html 
+# @app.route("/show")
+# def show_user_info():
+#     print("?+?+?+?+?+?+?+?+?+?+?+?+?+?+?+?+?+?+?+?" + session["user"]) #10:00 forgot to put user in ""
+#     # return render_template("show.html") #commented out at 72 15:17
+# # 72 11:50 Session is empty by default.  12:15  Session is a big empty box,  it only stores something if we tell it to
+# # 72 15:48 substitued 2 above for 1 below:
+#     return render_template("show.html", user_name = session["user"]) 
+#     # where is ABOVE user_name hooked up?
+#     # Me 8:49pm is it lines 21 & 36, 18-b_counter show.html 
 
 
 # /////////////////////////////////////////////////
@@ -84,10 +67,19 @@ def show_user_info():
 # /////////////////////////////////////////////////
 
 # 73 2:12 added below
-@app.route("/reset")
-def reset_session():
+# @app.route("/reset")
+# def reset_session():
+#     session.clear()
+#     return redirect("/") #==> to line 14 server.py?
+#     # SO    1.  username stays unless
+#     #       2.  unless "Reset Session" is clicked.
+#     #           2a TRK: <a> tag on line 25, 73-b_session_counter show.html
+#             # 73 3:41 this is like logging someone out
+
+@app.route("/destroy_session")
+def destroy_session():
     session.clear()
-    return redirect("/nero") #==> to line 14 server.py?
+    return redirect("/") #==> to line 14 server.py?
     # SO    1.  username stays unless
     #       2.  unless "Reset Session" is clicked.
     #           2a TRK: <a> tag on line 25, 73-b_session_counter show.html
