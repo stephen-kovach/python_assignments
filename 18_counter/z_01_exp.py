02bbdae5b2b30f86cc13f0865394986dbbf68d48
stephen-kovach <smkovach5458@gmail.com>
2022-05-15 21:06:21 -0500

    demo 019 edit server.py

--- 18_counter/z_01_exp.py
+++ 18_counter/z_01_exp.py
@@ -14,26 +14,23 @@ app.secret_key = 'keep it secret, keep it safe--from mordor mwahahaha' # set a s
 @app.route('/')  
 def index():
 
-    # print("========================Beginning") # Me 4:19 prints in terminal
-    # # ME 5:55pm where is below code hooked up?  line 18-47-72, 18-a_counter index.html
-    # if 'user' in session:  # Me 6:41pm changed 19 and 22 to user_a--still works
-    #     print('user exists!')
-    # else:
-    #     print("key 'user' does NOT exist")
-    #     # Me 4:26pm what input would trigger the above else statement to print?
-    # print("========================End") # Me 5:57pm track when prints in terminal 
+    print("========================Beginning") # Me 4:19 prints in terminal
+    # ME 5:55pm where is below code hooked up?  line 18-47-72, 18-a_counter index.html
+    if 'user' in session:  # Me 6:41pm changed 19 and 22 to user_a--still works
+        print('user exists!')
+    else:
+        print("key 'user' does NOT exist")
+        # Me 4:26pm what input would trigger the above else statement to print?
+    print("========================End") # Me 5:57pm track when prints in terminal 
 
 
     if "count" in session:
         session["count"] +=1  # this will increment by 1
     else:
-        session["count"] = 1
+        session["count"] = 0
 
-# TOGGLE
-    # return render_template("index.html", count = session["count"]) 
-    # is ABOVE code hooked up to line 16, 18-a_counter index.html?
-    return render_template("z02_index.html", count = session["count"]) 
-    # is ABOVE code hooked up to line 16, 18-counter z02_index.html?
+    return render_template("index.html", count = session["count"]) 
+    # is ABOVE code hooked up to line 17, 18-a_counter index.html?
 
 # BELOW KWV 22:40  are users designed to post catch data?.  NCV Methods with an 's'
 @app.route('/users', methods=["post"]) 
@@ -60,7 +57,7 @@ def process_user():
     # What does above code do?
     
     # 72 8:42 new edit for 5ish below
-    return redirect("/show") #==> to line 71 server.py?
+    return redirect("/show")
     
 
 # /////////////////////////////////////////////////
@@ -76,7 +73,6 @@ def show_user_info():
 # 72 15:48 substitued 2 above for 1 below:
     return render_template("show.html", user_name = session["user"]) 
     # where is ABOVE user_name hooked up?
-    # Me 8:49pm is it lines 21 & 36, 18-b_counter show.html 
 
 
 # /////////////////////////////////////////////////
@@ -87,7 +83,7 @@ def show_user_info():
 @app.route("/reset")
 def reset_session():
     session.clear()
-    return redirect("/nero") #==> to line 14 server.py?
+    return redirect("/")
     # SO    1.  username stays unless
     #       2.  unless "Reset Session" is clicked.
     #           2a TRK: <a> tag on line 25, 73-b_session_counter show.html
