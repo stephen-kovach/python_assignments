df1615ad51ab4cf19e06a18a5f0e5bf7aa6149c3
stephen-kovach <smkovach5458@gmail.com>
2022-05-17 22:37:54 -0500

    030 dojo survey

--- 20_dojo_survey/server.py
+++ 20_dojo_survey/server.py
@@ -1,4 +1,20 @@
-# I AM 19_great_number_game server.py
+# I AM 20_dojo_survey server.py
+
+# HEY! Post to GitHub Every Hour -->
+# Last update at 10:36pm -->
+
+# JSTOR====FLower and Hayes, Cognitive Process Model 1981
+
+# The diachotomy of control.
+    # Like restaurant work--you can only move so fast
+        # the key is getting organized
+
+# I can't control how fast I learn
+# All I can control is the time I put in
+    # Napoleon:  Space I can recover.  Time, never
+    # Trade Space for Time
+
+# Keep designing and evaluating mnmeonic strategies
 
 # Below Code:  not sure what this is telling me
 # from flask --try to define these terms later
@@ -8,25 +24,25 @@
 # session -got it
 # request--what is it?  I don't think I need it
 # why are we writing flask twice?
-
 from flask import Flask, render_template, redirect, session, request
-# Below Code:  not sure what this is telling me
 
-#
+
+# Below Code:  not sure what this is telling me
 # app 
 # Flask different than flask and Flask on 12 .  
-# what is the name --is this similar to __init__?  
+# what is the __name__ --is this similar to __init__?  
 # Are there terms for these? 
 app = Flask(__name__)
 
 # Below Code: I know we need this.  What happends if we leave it out?
-app.secret_key = "machiavellihewhodesiresorattemptstoreforemthegovernmentofastateandwishestohaveitacceptedmustatleastretainthesemblenceofoldforms"
-
+app.secret_key = "spaceicanrecovertimenever"
 @app.route("/")
 def index():
 
     return render_template('index.html') 
 
+
+# STOP HEY!  -I was about to Copy Below Code.  One route per form
 
 # CATCHing POST from form index.html
 @app.route('/process_guess', methods=["POST"])
