a7180757f2bfabda14dfe78500551763c6794747
stephen-kovach <smkovach5458@gmail.com>
2022-06-07 14:29:40 -0500

    for_loop_basic_01

--- 02_for_loop_basic_1/for_loop_basic_01.py
+++ 02_for_loop_basic_1/for_loop_basic_01.py
@@ -2,23 +2,23 @@
 
 # 1.  Basic.  Print all integers from 0 to 150
 
-# for x in range(0, 151):
-#     print(x)
+for x in range(0, 151):
+    print(x)
 
 # 2.  Multiples of Five:  Print all the multiples of 5 from 5 to 1,000
 
-# for x in range (5, 1001):
-#     if x % 5 == 0:
-#         print(x)
+for x in range (5, 1001):
+    if x % 5 == 0:
+        print(x)
 
 # 3.  Counting, the Doojo Way.  Print integers 1 to 100.  If divisible by five, print "Coding" if divisible by 10, print "Codiing Dojo"
 
-# for x in range (1, 101):
-#     if x % 10 == 0:
-#         print('Coding Dojo')
-#     elif x % 5 ==0:
-#         print('Coding')
-#     else: print(x)
+for x in range (1, 101):
+    if x % 10 == 0:
+        print('Coding Dojo')
+    elif x % 5 ==0:
+        print('Coding')
+    else: print(x)
 
 # 4.  Whoa. That sucker's huge.  Add odd integers from 0 to 500,000 and print the final sum
 
