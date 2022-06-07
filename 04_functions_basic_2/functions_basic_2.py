ba8e580978a2485c4de9ecb36356f9ee12ea8852
stephen-kovach <smkovach5458@gmail.com>
2022-06-07 14:38:14 -0500

    redo

--- 04_functions_basic_2/functions_basic_2.py
+++ 04_functions_basic_2/functions_basic_2.py
@@ -36,32 +36,32 @@
 
 # 3 First Plus Length
 
-# def first_plus_length(list):
-#     return list[0] + len(list)
+def first_plus_length(list):
+    return list[0] + len(list)
     
-# list = [1, 2, 3, 4, 5, 6, 7, 8]
-# print(first_plus_length(list))
+list = [1, 2, 3, 4, 5, 6, 7, 8]
+print(first_plus_length(list))
 
-# # which is better?  above or below?  If below, why do we not need to define the list?
+# which is better?  above or below?  If below, why do we not need to define the list?
 
-# print(first_plus_length([1, 2, 3, 4, 5]))
+print(first_plus_length([1, 2, 3, 4, 5]))
 
 
 # # 4 Values Greater Than Second
 
-# def values_second(list):
-#     if len(list) < 2:
-#         return False
-#     output = []
-#     for i in range(0, len(list)):
-#         if list[i] > list[1]:
-#             output.append(list[i])
-#     print(len(list))
-#     return output
+def values_second(list):
+    if len(list) < 2:
+        return False
+    output = []
+    for i in range(0, len(list)):
+        if list[i] > list[1]:
+            output.append(list[i])
+    print(len(list))
+    return output
 
-# print(values_second([1, 5, 3, 10, 4, 8, 9, 11]))
-# print(values_second([2]))
-# print(values_second([2, 1, 3]))
+print(values_second([1, 5, 3, 10, 4, 8, 9, 11]))
+print(values_second([2]))
+print(values_second([2, 1, 3]))
 
 # # 5.  This length, that value
 
